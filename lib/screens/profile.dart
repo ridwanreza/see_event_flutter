22347d4fba448ea263695794b80f89f01aa47d7b
kikiprayudi <kikipryd@gmail.com>
2022-01-13 16:42:01 +0700

    update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,98 +12,138 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  var buttonList = <dynamic>[];
+
+  @override
+  void initState() {
+    buttonList.addAll([
+      {
+        "icon": Icons.person,
+        "text": "Edit Profile",
+        "action": () {
+          Navigator.pushNamed(context, '/contact');
+        },
+      },
+      {
+        "icon": Icons.person,
+        "text": "Edit Password",
+        "action": () {
+          Navigator.pushNamed(context, '/contact');
+        },
+      },
+      {
+        "icon": Icons.event,
+        "text": "Saved Events",
+        "action": () {
+          Navigator.pushNamed(context, '/contact');
+        },
+      },
+      {
+        "icon": Icons.logout,
+        "text": "Log out",
+        "action": () {
+          Navigator.pushReplacementNamed(context, '/welcome');
+        },
+      },
+    ]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile',
-        style: GoogleFonts.notoSans(
-          color: Colors.white,
-          fontSize: 20,
-          fontWeight: FontWeight.w700,
-          ),
-        ),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Color(0xFF0E1D25),
-                Color(0xFF214457),
-              ]
-            )
-          ),
-        ),
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Color(0xFFECEEEF),
-        child: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.only(top: 32),
-            width: MediaQuery.of(context).size.width * 0.80,
-            child: Column(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.80,
-                  height: 225,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                  ),
-                  child: Center(
-                    child: Container(
-                      margin: EdgeInsets.only(top: 10, bottom: 20),
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 65,
-                            backgroundImage: AssetImage('lib/assets/person.png'),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 10),
-                            child: Text('Ridwan Reza Ramadhan',
-                              style: GoogleFonts.notoSans(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                                color: Color(0xFF373737),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 6),
-                            child: Text('ridwanreza77@gmail.com',
-                            style: GoogleFonts.notoSans(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xFF373737)
-                            ),),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 25),
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: Text('edit profile')),
-                      ElevatedButton(onPressed: () {}, child: Text('edit profile')),
-                      ElevatedButton(onPressed: () {}, child: Text('edit profile'))
-                    ],
-                  ),
-                )
-              ],
+        appBar: AppBar(
+          title: Text(
+            'Profile',
+            style: GoogleFonts.notoSans(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
             ),
           ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                  Color(0xFF0E1D25),
+                  Color(0xFF214457),
+                ])),
+          ),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
         ),
-      )
-    );
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Color(0xFFECEEEF),
+          child: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.only(top: 32),
+              width: MediaQuery.of(context).size.width * 0.80,
+              child: Column(
+                children: [
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.80,
+                      height: 225,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 65,
+                                backgroundImage:
+                                    AssetImage('lib/assets/person.png'),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 10),
+                                child: Text(
+                                  'Ridwan Reza Ramadhan',
+                                  style: GoogleFonts.notoSans(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xFF373737),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 6),
+                                child: Text(
+                                  'ridwanreza77@gmail.com',
+                                  style: GoogleFonts.notoSans(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color(0xFF373737)),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )),
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: buttonList.length,
+                      itemBuilder: (context, index) {
+                        var button = buttonList[index];
+                        return ElevatedButton(
+                          onPressed: button["action"],
+                          child: Text(button["text"]),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
